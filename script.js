4427992865bdc7bd9c23439357219ddbf2d9a258
Skillsebas <skillthink1@gmail.com>
2024-08-26 18:04:41 -0500

    Creando desafio

---
+++ script.js
@@ -0,0 +1,74 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value ="";
+    mensaje.style.backgroundImage = "none";
+}
+
+function encriptar (stringEncriptada){
+    let matrizCodigo = [["e", "enter" ], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+
+for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1])
+        }
+
+    }
+    return stringEncriptada
+}
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value ="";
+
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter" ], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+
+for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo [i][1] ,matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesencriptada
+}
+
+
+function validar(textArea) {
+  //expresion regular para minusculas y espacios
+    const validos = /^[a-z\sñ]*$/;
+    if (!validos.test(textArea)) {
+    mostrarNotificacion("Solo se permiten letras minusculas y espacios");
+    return false;
+}
+    return true;
+}
+
+
+function copiar(){
+    const texto = document.querySelector(".mensaje").value; // Obtiene el valor del textarea
+    navigator.clipboard.writeText(texto).then(function() {
+        alert("Texto copiado al portapapeles");
+    }).catch(function() {
+        alert("Hubo un error al copiar el texto");
+    });
+};
